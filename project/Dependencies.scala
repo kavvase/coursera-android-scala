c94bbb9e79db890053ef99a91002bbc5be5cd560
Hidetoshi Kawase <kavvase@gmail.com>
2015-03-29 02:23:44 +0900

    modify library versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   def compile(deps: ModuleID*): Seq[ModuleID] = deps map (_ % "compile")
 
-  val proguard   = "net.sf.proguard" %  "proguard-base" % "5.0"
+  val proguard   = "net.sf.proguard" %  "proguard-base" % "5.2"
   val scaloid    = "org.scaloid"     %% "scaloid"       % "3.6.1-10"
 
 }
